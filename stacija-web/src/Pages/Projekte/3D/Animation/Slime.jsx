e27a0c938a5513a8291686b4695abd142839c6fe
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2023-01-26 23:42:53 +0100

    .

--- stacija-web/src/Pages/Projekte/3D/Animation/Slime.jsx
+++ stacija-web/src/Pages/Projekte/3D/Animation/Slime.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import SlimeVid from "../../../../assets/3d/Animations/Videos/Slime.mp4";
+import Explanation from '../../../../assets/3d/Animations/explenation.png'
+import Footer from "../../../../components/Footer";
+import VideoBG from "../../../../components/Video/VideoBG";
+import "../../../../Styles/Animations.css";
+import "../../../../Styles/Projekte.scss";
 
 export default function Slime() {
   return (
-    <div>Slime</div>
-  )
+    <>
+      <section id="entry-cubes">
+        <h1 className="slimy">Slimy</h1>
+        <div className="cubes-container-description">
+          <p>
+            Eine meiner kompliziertesten Animationen ist die hier genannte
+            "Slime Animation". Ich habe versucht, etwas Abstraktes zu
+            animieren, das cool und verspielt aussieht.
+          </p>
+        </div>
+      </section>
+      <section id="entry-cubes">
+        <div className="cubes-container">
+          <VideoBG source={SlimeVid} />
+        </div>
+        <div className="cubes-container">
+          <img className="explenation-img" src={Explanation} />
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
